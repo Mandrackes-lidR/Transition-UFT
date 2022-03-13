3b439a03acef2aba536a109f6856965abefa2151
Simtrami <me@simtrami.net>
2022-03-13 22:56:52 +0100

    Minor edits to the signature verification process

--- resources/lang/en/mail.php
+++ resources/lang/en/mail.php
@@ -2,8 +2,11 @@
 
 return [
     'subject' => 'Confirm your email',
-    'greeting' => 'Hi!',
+    'greeting' => 'Thank you :firstName!',
     'confirm' => 'Click this button to validate your signature and have your name displayed in the signatories list.',
     'button' => 'Confirm my email address',
-    'dismiss' => "If you did not sign or do not want to appear on the list, you can ignore this message."
+    'dismiss' => "If you did not sign or do not want to appear on the list, you can ignore this message.",
+    'salutation' => "Regards,<br>
+:name",
+    'subcopy' => "If you're having trouble clicking the \":actionText\" button, copy and paste the URL below into your web browser:",
 ];
--- resources/lang/fr/mail.php
+++ resources/lang/fr/mail.php
@@ -2,8 +2,11 @@
 
 return [
     'subject' => 'Confirmez votre email',
-    'greeting' => 'Salut !',
-    'confirm' => 'Cliquez sur le bouton pour confirmer votre signature et apparaître sur la liste.',
+    'greeting' => 'Merci :firstName !',
+    'confirm' => 'Cliquez sur le bouton pour confirmer votre signature et apparaître sur la liste des signataires.',
     'button' => 'Confirmer mon email',
-    'dismiss' => "Si vous n'avez pas signé ou ne voulez pas apparaître dans la liste, vous pouvez ignorer ce message."
+    'dismiss' => "Si vous n'avez pas signé ou ne voulez pas apparaître dans la liste, vous pouvez ignorer ce message.",
+    'salutation' => "Cordialement,<br>
+:name",
+    'subcopy' => "Si vous avez des difficultés à cliquer sur le bouton \":actionText\", copiez-collez l'URL suivant dans votre navigateur :",
 ];
