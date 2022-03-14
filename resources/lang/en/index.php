223dad217fcde8dd3a717b2661663dfe97261f3e
Simtrami <me@simtrami.net>
2022-03-14 22:37:43 +0100

    Fix title

--- resources/lang/en/index.php
+++ resources/lang/en/index.php
@@ -2,7 +2,7 @@
 
 return [
     'page_title' => "Manifesto - :app_name",
-    'title' => "A web template<br>for ecological awakenings",
+    'title' => "Student proposal for a socio-ecological transition plan for the Federal University of Toulouse",
     'subtitle' => [
         'already' => 'Already',
         'sign_count' => ":count people signed!",
