313560f25e294f4fcc3f291a4161605bcdcdeac9
Simtrami <me@simtrami.net>
2022-03-14 01:29:04 +0100

    Integrate the new manifesto

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -38,10 +38,11 @@
 
         <section class="w-full mb-12 md:text-lg" id="manifesto">
             <!-- Headline -->
-            <header class="bg-theme text-white py-10 px-4 md:px-20 lg:px-0">
+            <header class="bg-theme text-white text-justify py-10 px-4 md:px-20 lg:px-0">
                 <p class="mx-auto max-w-4xl">
-                    Sunt amores manifestum audax, neuter acipenseres. Going to the mind doesn’t hurt joy anymore than
-                    inventing creates outer stigma. Why does the ferengi warp?
+                    Nous sommes un groupe d’étudiant.e.s présent.e.s dans les différentes écoles de l’université
+                    fédérale. Nous venons d’universités, d’écoles d’ingénieurs, de commerce, de SciencesPo... <u>Nous
+                        représentons la diversité des établissements de l’Université Fédérale de Toulouse</u>.
                 </p>
             </header>
             <!-- Socials -->
@@ -75,62 +76,197 @@
                 </div>
             </aside>
             <!-- Manifesto -->
-            <article class="px-4 mt-12 md:-mt-44 md:pr-8 md:pl-20 lg:px-0">
+            <article class="text-justify px-4 mt-12 md:-mt-44 md:pr-8 md:pl-20 lg:px-0">
                 <div class="mx-auto max-w-4xl">
                     <p class="mt-5">
-                        Molestiae dolor dolorum enim praesentium sit voluptas qui sapiente. Dolorem odio consequatur
-                        perferendis ad quo aut quisquam non. Esse expedita repellat temporibus sed fugit voluptatum.
-                        Deleniti atque et consequatur quia suscipit. Illo debitis officiis dolorem accusamus et corporis
-                        nihil. Distinctio qui esse est.
+                        En tant qu’étudiantes et étudiants, nous nous sentons particulièrement concerné.e.s par les
+                        questions liées aux enjeux socio-écologiques et nous considérons qu’elles ne sont pas prises en
+                        compte de façon sérieuse au sein de l’enseignement supérieur et notamment de&nbsp;:
                     </p>
-                    <h2 class="section-title mt-8">Sunt verpaes reperire fatalis, varius abactores.</h2>
-                    <p class="mt-5">
-                        Aliquid ea et et et non et repellendus enim. Consequatur sint delectus in. Placeat quae est et
-                        eveniet. Excepturi optio eum eveniet iste aut eum.
-                    </p>
-                    <p class="mt-5">
-                        Velit accusantium cum est vero sed maiores. Maxime sed id voluptatem quaerat libero nobis.
-                        Tenetur dolore quam aperiam expedita qui omnis temporibus. Vitae ducimus quia consequatur et
-                        facere delectus cumque.
-                    </p>
-                    <p class="mt-5">
-                        Id dolor sit voluptas aut blanditiis ipsum distinctio et. Ducimus ullam sit natus id ducimus.
-                        Quidem vero blanditiis ut error perferendis quae. Ab maxime ut sequi rerum explicabo accusamus
-                        ipsam sed.
-                    </p>
-                    <h2 class="section-title mt-8">Camerarius eleates foris demittos humani generis est.</h2>
-                    <ul class="list-disc marker:text-theme list-inside mt-5">
-                        <li>Yarr, haul me bung hole, ye sunny shipmate!</li>
-                        <li>Arg, weird parrot. go to port royal.</li>
-                        <li>Arg, passion!</li>
-                        <li>Why does the mast stutter?</li>
-                        <li>Golly gosh, wet strength!</li>
+                    <ul class="list-disc marker:text-theme dark:marker:text-theme-light pl-4 space-y-2 mt-5 md:pl-6 lg:pl-7">
+                        <li>
+                            <strong class="text-theme dark:text-theme-light">nos cursus étudiants</strong>&nbsp;:
+                            sommes-nous formé.e.s de façon cohérente face à ces enjeux&nbsp;? Les différentes entités1
+                            qui se sont penchées dessus penchent plutôt pour le non (encore tout récemment <a
+                                href="https://start.lesechos.fr/apprendre/universites-ecoles/exclusivite-classement-2021-des-ecoles-et-universites-pour-changer-le-monde-quels-sont-les-30-etablissements-les-mieux-classes-1357899"
+                                target="_blank"
+                            >le classement des Echos Start</a> nous confirme cette tendance). Aurons-nous les
+                            compétences pour répondre à ces mêmes problématiques&nbsp;? Encore une fois, la réponse est
+                            loin d’être évidente
+                        </li>
+                        <li>
+                            <strong class="text-theme dark:text-theme-light">nos débouchés professionnels</strong>&nbsp;:
+                            trouverons-nous un métier qui soit en phase avec nos convictions&nbsp;? Aura-t-il du sens&nbsp;?
+                            Pour quelle utilité sociétale&nbsp;?
+                        </li>
+                        <li>
+                            <strong class="text-theme dark:text-theme-light">notre citoyenneté</strong>&nbsp;: face au
+                            lot de catastrophes qui nous est promis, nous ne pouvons rester inactif.ve.s. Il est crucial
+                            que nous agissions pour une transformation de la société. Il est temps maintenant d’aider
+                            collectivement à la transition de nos établissements !
+                        </li>
+                        <li>
+                            <strong class="text-theme dark:text-theme-light">son exemplarité</strong>&nbsp;: que penser
+                            d’établissements qui ne prennent même pas la peine de respecter le peu de réglementations
+                            environnementales en vigueur (ex&nbsp;: publier son bilan d’émission de gaz à effet de serre
+                            - BEGES - tous les 3 ans) alors qu’il faudrait non pas mesurer mais réduire (de 5% par an)
+                            ces émissions&nbsp;?
+                        </li>
                     </ul>
                     <p class="mt-5">
-                        Nullam at purus ut lectus consequat aliquam non vitae odio. Proin purus lacus, imperdiet vel
-                        varius vitae, tincidunt ut justo. Nunc eget dapibus eros, sed maximus orci. Nam lobortis commodo
-                        faucibus. Suspendisse ac purus eros. Donec vel tortor nec risus condimentum ultricies non luctus
-                        diam. Morbi nibh nisi, sodales id mattis in, varius quis dolor. Vestibulum ante ipsum primis in
-                        faucibus orci luctus et ultrices posuere cubilia curae; Suspendisse sit amet leo cursus erat
-                        ornare interdum ac et felis.
+                        Face à ce constat, nous avons souhaité faire <strong>13 propositions pour accélérer la
+                            transition de l’ensemble de l’UFT</strong>. Nous sommes conscient.e.s de la complexité de
+                        cette tâche au vu de la diversité des établissements (que ce soit en taille, thématique,
+                        gouvernance). Toutefois, la <strong>complexité ne doit pas être une excuse à notre
+                            inaction</strong>.
                     </p>
-                    <h2 class="section-title mt-8">Ubi est festus verpa?</h2>
+                    <h2 class="section-title mt-8">Les 5 actions fondamentales pour créer une dynamique de transition
+                        globale</h2>
                     <p class="mt-5">
-                        Nulla sit amet enim id eros malesuada porta. Phasellus porttitor posuere urna, id facilisis sem
-                        euismod nec. Nunc a dolor et enim congue commodo. Donec pharetra commodo dolor, sed euismod
-                        libero auctor at. Praesent vel risus at diam pretium maximus in id turpis. In hac habitasse
-                        platea dictumst. Praesent consectetur ex a posuere interdum. Duis a aliquam nunc, at condimentum
-                        tellus. Integer nec sollicitudin ipsum. Sed malesuada sapien quis erat fringilla hendrerit.
-                        Donec sit amet pretium mauris. Fusce imperdiet laoreet justo, ac dictum nisl laoreet eget. Donec
-                        ut lacus eget nisi fermentum bibendum.
+                        La transition ne se fera que si les directions sont convaincues de la nécessité d’impulser un
+                        changement face aux enjeux, que le personnel adhère à ce projet de transformation et que des
+                        moyens financiers et humains à la hauteur y soient dédiés. Nous avons déjà porté ces
+                        propositions auprès de l’Université fédérale de Toulouse (instance administrative regroupant les
+                        universités toulousaines) qui, malgré sa volonté de collaborer à ce sujet, n’a souhaité retenir
+                        que moins de la moitié de ces propositions. Or, pour être à la hauteur des enjeux, nous pensons
+                        que cela ne suffit pas. L’enjeu est à présent de créer un rapport de force à l’intérieur de nos
+                        établissements en étant le plus nombreux.ses à signer ce document pour avoir du poids et
+                        permettre ensuite de porter ces propositions au sein des instances décisionnelles de nos
+                        administrations. Lesdites propositions sont :
                     </p>
+                    <ol class="measure-list mt-5">
+                        <li>
+                            Former les directions de l’ensemble des établissements aux enjeux socio-écologiques en leur
+                            proposant une formation de 10h avant fin 2022.
+                        </li>
+                        <li>
+                            Former l’ensemble du personnel volontaire aux enjeux socio-écologiques.
+                        </li>
+                        <li>
+                            Publier une feuille de route par établissement avant fin 2022 présentant les actions qui
+                            seront mises en place pour mener sa transition écologique.
+                        </li>
+                        <li>
+                            Soutenir ces transformations en y allouant des moyens humains et financiers permettant de
+                            les structurer, de les suivre et finalement de les réaliser.
+                        </li>
+                        <li>
+                            Créer un comité écologique dans chaque établissement associant direction, corps professoral,
+                            personnel administratif et technique et étudiants.
+                        </li>
+                    </ol>
+                    <h2 class="section-title mt-8">Transformer la formation</h2>
                     <p class="mt-5">
-                        Suspendisse efficitur orci nulla, a rhoncus lorem auctor et. Sed ac lorem et tortor tincidunt
-                        venenatis et quis odio. Quisque elit odio, tempus in maximus a, egestas non erat. Praesent
-                        rutrum libero risus, vel accumsan magna commodo nec. Quisque non suscipit augue. Morbi blandit
-                        risus ex, at molestie metus sollicitudin in. Mauris luctus risus at orci hendrerit auctor. Fusce
-                        mattis metus a ultricies euismod.
+                        Pour faire la transition, il nous faudra comprendre le constat planétaire, les conséquences que
+                        cela aura sur notre société, sur nous et identifier les causes (politiques, économiques,
+                        historiques) pour tenter d’y apporter une réponse. Une fois ce constat compris, il est
+                        indispensable d’adapter l’ensemble des filières à celui-ci.
                     </p>
+                    <ol class="measure-list mt-5" start="6">
+                        <li>
+                            Sensibiliser aux enjeux de la transition socio-écologique 100% des personnes inscrites dans
+                            l’établissement lors de leur première année d’étude à l’aide d’approches pédagogiques
+                            dédiées.
+                        </li>
+                        <li>
+                            Inclure dans l’intégralité des formations des établissements un tronc commun
+                            interdisciplinaire obligatoire de 30h sur les enjeux socio-écologiques valorisé par des
+                            crédits ECTS.
+                        </li>
+                        <li>
+                            Adapter les enseignements de chaque filière au regard des enjeux sociaux et
+                            environnementaux.
+                        </li>
+                    </ol>
+                    <h2 class="section-title mt-8">Valoriser la vie étudiante</h2>
+                    <p class="mt-5">
+                        Donner la possibilité à tous de s’émanciper, se questionner en dehors de son cursus pour relier
+                        études du supérieur et citoyenneté.
+                    </p>
+                    <ol class="measure-list mt-5" start="9">
+                        <li>
+                            Permettre à chaque étudiant.e de s'engager dans des activités à vocation socio-écologique et
+                            de participer à la construction du monde durable de demain.
+                        </li>
+                        <li>
+                            Faciliter et valoriser les césures axées sur la transition écologique et sociale notamment à
+                            travers des retours d’expériences.
+                        </li>
+                    </ol>
+                    <h2 class="section-title mt-8">Être exemplaire</h2>
+                    <p class="mt-5">
+                        La transition écologique ne peut être une action marginale ou silotée, elle nécessite de
+                        transformer nos institutions. C’est dans ce cadre que nous souhaitons que nos universités soient
+                        exemplaires en réduisant leurs impacts environnementaux et plus largement en adaptant l’ensemble
+                        de son activité.
+                    </p>
+                    <ol class="measure-list mt-5" start="11">
+                        <li>
+                            Réduire la quantité de déchets à la source (papiers, gobelets, canettes) et rendre
+                            obligatoire le tri sélectif sur l’ensemble des campus de l’UFT.
+                        </li>
+                        <li>
+                            Élaborer et suivre des stratégies bas carbone sur l’ensemble des campus suivant la
+                            méthodologie proposée par l’Accord de Grenoble.
+                        </li>
+                        <li>
+                            Avoir une communication honnête et transparente sur l'état de la transition et les actions
+                            entreprises.
+                        </li>
+                    </ol>
+                </div>
+            </article>
+            <article class="text-justify px-4 mt-12 md:pr-8 md:pl-20 lg:px-0">
+                <div class="mx-auto max-w-4xl">
+                    <hr/>
+                    <h2 class="text-theme-dark dark:text-theme-light dark:selection:bg-theme font-title font-bold text-2xl md:text-3xl mt-8">
+                        Sources</h2>
+                    <p class="mt-5">
+                        Nos 13 propositions se basent sur les différents travaux et rapports qui ont été publiés sur la
+                        transition socio-écologique de l’enseignement supérieur. Nous nous sommes notamment basé.e.s sur&nbsp;:
+                    </p>
+                    <ul class="list-disc marker:text-theme pl-5 mt-5">
+                        <li>
+                            <a class="underline text-theme dark:text-theme-light transition hover:text-theme-dark dark:hover:text-theme"
+                               href="https://refedd.org/cne/" target="_blank"
+                            >La consultation nationale étudiante par le RESES en 2020</a></li>
+                        <li>
+                            <a class="underline text-theme dark:text-theme-light transition hover:text-theme-dark dark:hover:text-theme"
+                               href="https://theshiftproject.org/lavenir-de-la-planete-dans-lenseignement-superieur/"
+                               target="_blank"
+                            >Les travaux du Shift Project sur l’enseignement supérieur</a></li>
+                        <li>
+                            <a class="underline text-theme dark:text-theme-light transition hover:text-theme-dark dark:hover:text-theme"
+                               href="https://pour-un-reveil-ecologique.org/fr/reveiller-sa-formation/" target="_blank"
+                            >Les travaux et publication de l’association “Pour un Réveil Ecologique”</a></li>
+                        <li>
+                            <a class="underline text-theme dark:text-theme-light transition hover:text-theme-dark dark:hover:text-theme"
+                               href="https://campus-transition.org/le-manuel-de-la-grande-transition/" target="_blank"
+                            >Le Manuel de la Grande Transition</a></li>
+                        <li>
+                            <a class="underline text-theme dark:text-theme-light transition hover:text-theme-dark dark:hover:text-theme"
+                               href="https://cache.media.enseignementsup-recherche.gouv.fr/file/Actus/05/7/Rapport_mission_Jouzel_1394057.pdf"
+                               target="_blank"
+                            >Les conclusions du Rapport Jouzel (Mars 2021)</a> et le nouveau <a
+                                class="underline text-theme dark:text-theme-light transition hover:text-theme-dark dark:hover:text-theme"
+                                href="https://www.enseignementsup-recherche.gouv.fr/fr/rapport-former-aux-enjeux-de-la-transition-ecologique-dans-le-superieur-83888"
+                                target="_blank"
+                            >rapport publié en Février 2022</a></li>
+                        <li>
+                            <a class="underline text-theme dark:text-theme-light transition hover:text-theme-dark dark:hover:text-theme"
+                               href="https://jirafeau.isae-supaero.fr/f.php?h=2QnVpblY" target="_blank"
+                            >L’accord de Grenoble (de la COP2 Étudiante)</a></li>
+                        <li>Des articles notamment affiliés à des personnels de nos établissements toulousains&nbsp;: <a
+                                class="underline text-theme dark:text-theme-light transition hover:text-theme-dark dark:hover:text-theme"
+                                href="https://aoc.media/opinion/2021/10/28/pour-un-enseignement-de-la-transition-ecologique/"
+                                target="_blank"
+                            >pour un enseignement de la transition écologique</a>, <a
+                                class="underline text-theme dark:text-theme-light transition hover:text-theme-dark dark:hover:text-theme"
+                                href="https://www.linkedin.com/pulse/la-transition-%25C3%25A9cologique-de-lenseignement-sup%25C3%25A9rieur-quelle-mugnier"
+                                target="_blank"
+                            >sur la transition de l’enseignement supérieur</a>...
+                        </li>
+                    </ul>
                 </div>
             </article>
         </section>
