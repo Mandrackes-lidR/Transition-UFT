8dba20b91d1a265fe951e663434c06a3f855b339
Simtrami <me@simtrami.net>
2022-03-14 23:13:17 +0100

    Try fixing TLS issues in Clever Cloud

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use Fideloper\Proxy\TrustProxies as Middleware;
+use Illuminate\Config\Repository;
 use Illuminate\Http\Request;
 
 class TrustProxies extends Middleware
@@ -20,4 +21,12 @@ class TrustProxies extends Middleware
      * @var int
      */
     protected $headers = Request::HEADER_X_FORWARDED_ALL;
+
+    public function __construct(Repository $config)
+    {
+        if ($trustedProxies = $_SERVER['CC_REVERSE_PROXY_IPS'] ?? null) {
+            $this->proxies = array_merge(['127.0.0.1'], explode(',', $trustedProxies));
+        }
+        parent::__construct($config);
+    }
 }
